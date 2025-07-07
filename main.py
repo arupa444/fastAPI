facf6becf82ed9c1fd93040973f945a7a9cb2a8c
Arupa Nanda Swain <140882921+arupa444@users.noreply.github.com>
2025-07-08 01:47:56 +0530

    initial commit

--- main.py
+++ main.py
@@ -30,4 +30,16 @@ def fetchOneP(p_id: str = Path(..., description='Enter your patient id here....'
     raise HTTPException(status_code=404, detail='patient not found in DB')
 
 @app.get("/sort")
-def sortedData(sortBy: str= Query(..., description="Enter the field that you want to sort...", example="bmi"), ):
+def sortedData(sortBy: str= Query(..., description="The field that you want to sort...", example="bmi"), order: str= Query("asc", description="The order will be in ascending or decending order.", example="asc")):
+    vaild_fields = ["height", "weight", "bmi"]
+
+    if sortBy not in vaild_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid feild select from {vaild_fields}")
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Enter a valid input either asc or desc")
+    data = fetchData()
+
+    sorted(data.values(), key=lambda x: x[sortBy], reverse=True)
+
+    
+    
